3f65bb016896d16de1e6b688a86acde28332a609
JectaIne <68130184+JectaIne@users.noreply.github.com>
2020-11-11 22:45:43 +0100

    Update Ritchi.js
    Fixed something stupid.

--- Ritchi.js
+++ Ritchi.js
@@ -2,11 +2,11 @@ const config = require('./customization.json')
 const client = require('discord-rich-presence')(config.id);
 
 client.updatePresence({
-  state: '${config.title}',
-  details: '${config.subtitle}',
+  state: `${config.title}`,
+  details: `${config.subtitle}`,
   startTimestamp: Date.now(),
   endTimestamp: Date.now(),
-  largeImageKey: '${config.lIK}',
-  smallImageKey: '$config.sIK}',
+  largeImageKey: `${config.lIK}`,
+  smallImageKey: `${config.sIK}`,
   instance: true,
 });
