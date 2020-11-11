41dea1466aae5cdf74722907be225538a1a143e4
JectaIne <68130184+JectaIne@users.noreply.github.com>
2020-11-12 00:02:56 +0100

    Update Ritchi.js

--- Ritchi.js
+++ Ritchi.js
@@ -1,6 +1,8 @@
 const config = require('./customization.json')
 const client = require('discord-rich-presence')(config.id);
 
+if(config.fakegame === true) {
+
 client.updatePresence({
   state: config.title,
   details: config.subtitle,
@@ -10,3 +12,20 @@ client.updatePresence({
   smallImageKey: config.sIK,
   instance: true,
 });
+
+} else {
+
+client.updatePresence({
+  state: config.title,
+  details: config.subtitle,
+  startTimestamp: Date.now(),
+  endTimestamp: Date.now(),
+  largeImageKey: config.lIK,
+  smallImageKey: config.sIK,
+  instance: true,
+});
+
+  }
+}
+
+
