445cc4355600f536109a87cd83a126f04f237b4d
JectaIne <68130184+JectaIne@users.noreply.github.com>
2020-11-22 04:53:26 +0100

    v1.3
    Final result! For now....

--- Ritchi.js
+++ Ritchi.js
@@ -11,10 +11,6 @@ Discord.register(settings.id)
 const rpc = new Discord.Client({transport: "ipc"});
 
   rpc.on("ready", () => {
-    console.log("  ");
-    console.log("  ");
-    console.log("  ");
-    
     console.clear()
     console.log("  ");
     console.log("  ");
@@ -23,21 +19,33 @@ const rpc = new Discord.Client({transport: "ipc"});
     console.log("  ");
     console.log("  ");
     console.log(center(`
-██████  ██ ████████  ██████ ██   ██ ██ 
-██   ██ ██    ██    ██      ██   ██ ██ 
-██████  ██    ██    ██      ███████ ██ 
-██   ██ ██    ██    ██      ██   ██ ██ 
-██   ██ ██    ██     ██████ ██   ██ ██      
-    `.red, 118));
+
+
+    ██▓███   ██▀███   ▒█████   ▄▄▄██▀▀▀▓█████  ▄████▄  ▄▄▄█████▓    ██▀███   ██▓▄▄▄█████▓ ▄████▄   ██░ ██  ██▓
+    ▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒   ▒██   ▓█   ▀ ▒██▀ ▀█  ▓  ██▒ ▓▒   ▓██ ▒ ██▒▓██▒▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒▓██▒
+    ▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒   ░██   ▒███   ▒▓█    ▄ ▒ ▓██░ ▒░   ▓██ ░▄█ ▒▒██▒▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░▒██▒
+    ▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░▓██▄██▓  ▒▓█  ▄ ▒▓▓▄ ▄██▒░ ▓██▓ ░    ▒██▀▀█▄  ░██░░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ ░██░
+    ▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░ ▓███▒   ░▒████▒▒ ▓███▀ ░  ▒██▒ ░    ░██▓ ▒██▒░██░  ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓░██░
+    ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░  ▒▓▒▒░   ░░ ▒░ ░░ ░▒ ▒  ░  ▒ ░░      ░ ▒▓ ░▒▓░░▓    ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒░▓  
+    ░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░  ▒ ░▒░    ░ ░  ░  ░  ▒       ░         ░▒ ░ ▒░ ▒ ░    ░      ░  ▒    ▒ ░▒░ ░ ▒ ░
+    ░░         ░░   ░ ░ ░ ░ ▒   ░ ░ ░      ░   ░          ░           ░░   ░  ▒ ░  ░      ░         ░  ░░ ░ ▒ ░
+                ░         ░ ░   ░   ░      ░  ░░ ░                     ░      ░           ░ ░       ░  ░  ░ ░  
+                                               ░                                          ░                    
     
-      setTitle(`RITCHI | RPC IS RUNNING`);
-    
-    console.log(center("$───────────────────────────────$".cyan, 130));
-    console.log(center(`Title: ${settings.title}`.gray, 130));
-    console.log(center(`Subtitle: ${settings.subtitle}`.gray, 130));
-    console.log(center(`Fakegame: ${settings.fakegame}`.gray, 130));
-    console.log(center("$───────────────────────────────$".cyan, 130));
-    
+       
+    `.red, 120));
+    setTitle(`Project: Ritchi | リッチ`);
+    console.log(center("$───────────────────────────────$".cyan, 136));
+    console.log(center(`Id: ${settings.id}`.gray, 136));
+    console.log(center(`Title: ${settings.title}`.gray, 136));
+    console.log(center(`Subtitle: ${settings.subtitle}`.gray, 136));
+    console.log(center(`lIK: ${settings.lIK}`.gray, 136));
+    console.log(center(`lIK_Name: ${settings.lIK_name}`.gray, 136));
+    console.log(center(`sIK: ${settings.sIK}`.gray, 136));
+    console.log(center(`sIK_name: ${settings.sIK_name}`.gray, 136));
+    console.log(center(`Fakegame: ${settings.fakegame}`.gray, 136));
+    console.log(center("$───────────────────────────────$".cyan, 136));
+    console.log(center("$リッチ Made by ! EC JΞCTA 伊根#7417$".red, 130));
     console.log("  ");
     console.log("  ");
     console.log("  ");
